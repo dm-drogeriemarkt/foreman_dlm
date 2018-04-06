0b2ebf98ba4e0c2fbdb8fb5da75c340f034ad0a2
Timo Goebel <mail@timogoebel.name>
2018-04-06 17:52:58 +0200

    add travis-ci

--- test/test_plugin_helper.rb
+++ test/test_plugin_helper.rb
@@ -1,6 +1,8 @@
 # This calls the main test_helper in Foreman-core
 require 'test_helper'
 
+FactoryBot = FactoryGirl
+
 # Add plugin to FactoryBot's paths
 FactoryBot.definition_file_paths << File.join(File.dirname(__FILE__), 'factories')
 if ForemanDlm.with_monitoring?
