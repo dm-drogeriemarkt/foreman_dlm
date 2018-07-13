5a3093fbeef0fe396993b6ef2070624e86397737
Timo Goebel <mail@timogoebel.name>
2018-07-13 14:52:29 +0200

    list locks of a host

---
+++ test/controllers/hosts_controller_test.rb
@@ -0,0 +1,12 @@
+require 'test_plugin_helper'
+
+class HostsControllerTest < ActionController::TestCase
+  let(:host) { FactoryBot.create(:host, :with_dlm_facet) }
+
+  test '#show shows dlm locks of that host' do
+    FactoryBot.create_list(:dlmlock, 2, host: host)
+    get :show, params: { :id => host.to_param }, session: set_session_user
+    assert_response :success
+    assert @response.body.match(/id='pagelet-id-locks'/)
+  end
+end
