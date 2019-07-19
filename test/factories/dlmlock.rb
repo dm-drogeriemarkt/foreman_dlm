a995caf2c70ae8519bd080d38abd091a69d4d0df
Timo Goebel <mail@timogoebel.name>
2019-07-19 11:17:46 +0200

    use dynamic attrs for Factory Bot 5.x compat

--- test/factories/dlmlock.rb
+++ test/factories/dlmlock.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :dlmlock, class: ::ForemanDlm::Dlmlock::Update do
     sequence(:name) { |n| "Lock #{n}" }
-    type 'ForemanDlm::Dlmlock::Update'
+    type { 'ForemanDlm::Dlmlock::Update' }
 
     trait :locked do
       host
